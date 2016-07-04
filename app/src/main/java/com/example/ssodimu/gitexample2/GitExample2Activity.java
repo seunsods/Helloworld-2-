b19fa8d94a88756cb02169c92f16794978ed2d28
ssodimu <seunsods@gmail.com>
2016-07-04 15:09:51 +0100

    Initial Commit

---
+++ app/src/main/java/com/example/ssodimu/gitexample2/GitExample2Activity.java
@@ -0,0 +1,13 @@
+package com.example.ssodimu.gitexample2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GitExample2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_git_example2);
+    }
+}
